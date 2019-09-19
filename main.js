c74366a5b65daf589290b136a040bd0536a6c0fe
Weikai Wang <wc563388548@gmail.com>
2019-09-19 13:29:45 +0900

    add communications between main process and renderer process

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 
 // 'ready'代表electron已经完成加载并准备好创建BrowserWindow时调用
 // BrowserWindow只能在主进程中使用
@@ -13,16 +13,11 @@ app.on('ready', () => {
   });
   mainWindow.loadFile('./ui/index.html'); // 导入html去显示ui
   mainWindow.webContents.openDevTools(); //打开开发者工具
-
-  // 创建第二个窗口
-  /* const secondWindow = new BrowserWindow({
-      width: 200,
-      height: 100,
-      webPreferences: {
-        nodeIntegration: true, // nodeIntegration设置为true代表可以使用nodejs的API
-      },
-      parent: mainWindow, // 定义第二个窗口的父窗口, 当父窗口关闭子窗口也会随之关闭
-    });
-    mainWindow.loadFile('index.html'); // 导入html去显示ui
-  */
+  // 使用ipcMain接收从渲染进程发送过来的数据
+  ipcMain.on('message', (event, arg) => {
+    console.log('received message from renderer process: ', arg);
+    // 从主进程作出回复
+    // 也可以使用mainWindow来代替event.sender
+    event.sender.send('reply', 'hello from main');
+  });
 });
--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,7 +1,11 @@
-// Node Api
-alert(process.versions.node);
+const { ipcRenderer }  = require('electron');
 
 // DOM Api
 window.addEventListener('DOMContentLoaded', () => {
-    alert("DOMContentLoaded");
+    console.log("DOMContentLoaded");
+    // 从渲染进程向主进程发送数据
+    ipcRenderer.send('message', 'Hello from renderer process');
+    ipcRenderer.on('reply', (event, arg) => {
+        document.getElementById('message').innerHTML = arg;
+    });
 });
