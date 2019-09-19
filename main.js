324f0af86128a4410facb556bf2600a6ccfa2b4d
Weikai Wang <wc563388548@gmail.com>
2019-09-19 17:31:29 +0900

    optimize code

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 
+// 封装BrowserWindow类
+class AppWindow extends BrowserWindow{
+    constructor(config, fileLocation){
+        const basicConfig = {
+            width: 800,
+            height: 600,
+            webPreferences: {
+                nodeIntegration: true, // nodeIntegration设置为true代表可以使用nodejs的API
+            }
+        };
+        const finalConfig = {...basicConfig, ...config};
+        super(finalConfig);
+        this.loadFile(fileLocation); // 导入html去显示ui``
+        // 消除显示窗口时的视觉闪烁(显示未渲染画面)
+        this.once('ready-to-show', () => {
+            console.log("ready-to-show");
+            this.show();
+        });
+    }
+}
+
 // 'ready'代表electron已经完成加载并准备好创建BrowserWindow时调用
 // BrowserWindow只能在主进程中使用
 // 创建窗口
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true, // nodeIntegration设置为true代表可以使用nodejs的API
-    }
-  });
-  mainWindow.loadFile('./ui/index.html'); // 导入html去显示ui
+  const mainWindow = new AppWindow({}, './ui/index.html');
   // mainWindow.webContents.openDevTools(); //打开开发者工具
   ipcMain.on('openAddMusicWindow', () => {
       console.log("openAddMusicWindow");
-      const addMusicsWindow = new BrowserWindow({
+      const addMusicsWindow = new AppWindow({
         width: 500,
         height: 400,
         parent: mainWindow,
-        webPreferences: {
-          nodeIntegration: true, // nodeIntegration设置为true代表可以使用nodejs的API
-        }
-      });
-      addMusicsWindow.loadFile('./ui/add.html');
+      }, './ui/add.html');
   });
 
 });
